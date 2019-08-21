f57cd2f5227f614e38a5341ed2337335bee316e8
= <=>
2019-08-21 13:19:26 +0200

    Added client/server example

---
+++ client-server/static/presenter.js
@@ -0,0 +1,41 @@
+function presenter(view, model) {
+    "use strict";
+    
+    function showModel() {
+        if (model.winner) {
+            view.showWinner(model.winner)
+        } else if (model.stalemate) {
+            view.showStalemate()
+        } else {
+            view.showInTurn(model.inTurn)
+        }
+        view.updateBoard(model.board)
+    }
+
+    function clickBoard(x, y) {
+        const { gameNumber } = model
+        fetch('http://localhost:8080/move', { method: 'POST', body: JSON.stringify({ x, y, gameNumber }) })
+        .then(res => res.json())
+        .then(json => {
+            const { moves, inTurn, winner, stalemate } = json
+            moves.forEach(({x, y, player}) => model.board[x][y] = player)
+            Object.assign(model, { inTurn, winner, stalemate })
+            showModel()
+        })
+        .catch(console.error)
+    }
+
+    function clickReset() {
+        fetch('http://localhost:8080/clean', { method: 'POST' })
+        .then(res => res.json())
+        .then(json => {
+            model = json
+            showModel()
+        })
+        .catch(console.error)
+    }
+
+    showModel()
+
+    return { clickBoard, clickReset }
+}
---
+++ client-server/static/view.js
@@ -0,0 +1,50 @@
+function view() {
+    "use strict";
+    
+    const messages = document.getElementById('messages'),
+        table = document.getElementById('board'),
+        tiles = [ new Array(3), new Array(3), new Array(3) ],
+        reset = document.getElementById('reset')
+    
+    for (let i = 0; i < 3; i++) {
+        let tr = table.appendChild(document.createElement('tr'))
+        for (let j = 0; j < 3; j++) {
+            tiles[i][j] = tr.appendChild(document.createElement('td'))
+        }
+    };
+    
+    function showWinner(winner) {
+        messages.textContent = winner.winner + ' won!'
+    };
+    
+    function showStalemate() {
+        messages.textContent = "Stalemate!"
+    }
+    
+    function showInTurn(inTurn) {
+        messages.textContent = 'Your turn, ' + inTurn
+    }
+
+    function updateBoard(board) {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j]) {
+                    tiles[i][j].style['background-image'] = 'url(' + board[i][j] + '.png)'
+                } else {
+                    tiles[i][j].style.background = 'white'
+                }
+            }
+        };
+    }
+
+    function listen(listener) {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                tiles[i][j].onclick = listener.clickBoard.bind(listener, i, j)
+            }
+        }
+        reset.onclick = listener.clickReset
+    }
+
+    return { showWinner, showStalemate, showInTurn, updateBoard, listen }
+}
